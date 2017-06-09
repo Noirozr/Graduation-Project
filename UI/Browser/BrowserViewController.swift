84304d50f06edc4fa1d7e7a02371d64b9cd2d157
Noirozr <noirozr@gmail.com>
2017-06-09 15:36:22 +0800

    update

--- UI/Browser/BrowserViewController.swift
+++ UI/Browser/BrowserViewController.swift
@@ -9,11 +9,18 @@
 import UIKit
 
 class BrowserViewController: MATBaseViewController {
+    var mainCollectionView: UICollectionView?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Browser"
+        
+        self.p_constructSubviews()
         // Do any additional setup after loading the view.
     }
 
+    //MARK: - Construct Subviews
+    private func p_constructSubviews() {
+        
+    }
 }
